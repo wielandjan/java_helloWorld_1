e7cf3dbe8c062ca44f07b59c5da064d2caee3254
Jan-Wieland <jan.wieland@liebherr.com>
2024-10-29 17:10:10 +0100

    new file:   scr/exercises/T00_AddInt.java

---
+++ scr/exercises/T00_AddInt.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class T00_AddInt {
+    public static void main(String[] args) {
+        int i, j, result;
+        i = 999999999;
+        j = 10000;
+
+        result = i * j;
+        System.out.println(result);
+    }
+
+}
