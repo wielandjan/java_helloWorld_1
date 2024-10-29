f5899daf031493453010e25d9aafe74e5d39e9c3
Jan-Wieland <jan.wieland@liebherr.com>
2024-10-29 10:49:42 +0100

    modified:   scr/main/D08_CalculationsAndPseudoRandomNumbers.java

--- scr/main/D08_CalculationsAndPseudoRandomNumbers.java
+++ scr/main/D08_CalculationsAndPseudoRandomNumbers.java
@@ -2,21 +2,28 @@ package main;
 
 import java.util.Random;
 
+/**
+ * Class to demonstrate mathematical calculations and pseudo-random number
+ * generation.
+ * 
+ * @author Jan Wieland
+ * @version 1.0
+ */
 public class D08_CalculationsAndPseudoRandomNumbers {
     public static void main(String[] args) {
 
-        /* Mathematischen Berechnungen */
+        /* Mathematical calculations */
         double result = Math.sqrt(64);
         System.out.println("result: " + result);
 
-        /* Pseudozufallszahlen */
+        /* Pseudo-random numbers */
         int randomNumber;
 
-        // gpt("Cass Math")
+        // Using Math class to generate a random number between 0 and 99
         randomNumber = (int) (Math.random() * 100);
         System.out.println("randomNumber: " + randomNumber);
 
-        // gpt("Class Random")
+        // Using Random class to generate a random number between 1 and 100
         Random myRandom = new Random();
         randomNumber = myRandom.nextInt(1, 101);
         System.out.println("randomNumber: " + randomNumber);
