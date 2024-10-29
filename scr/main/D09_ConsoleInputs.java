7b05d90128facd8f5f46b98c8a18f820f92ad6f6
Jan-Wieland <jan.wieland@liebherr.com>
2024-10-29 11:24:11 +0100

    modified:   scr/main/D08_CalculationsAndPseudoRandomNumbers.java 	new file:   scr/main/D09_ConsoleInputs.java

---
+++ scr/main/D09_ConsoleInputs.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Scanner;
+
+public class D09_ConsoleInputs {
+
+    public static void main(String[] args) {
+
+        /* Scanner initialisieren */
+        Scanner myScanner = new Scanner(System.in);
+    }
+
+}
