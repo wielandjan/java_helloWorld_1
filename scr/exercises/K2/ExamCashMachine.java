3f8973fa23282847e29b6c4a18a874e6ff8b6164
Jan-Wieland <jan.wieland@liebherr.com>
2024-11-19 16:12:17 +0100

    new file:   scr/exercises/K2/ExamCashMachine.java 	new file:   scr/exercises/K2/ExamTask03.java

---
+++ scr/exercises/K2/ExamCashMachine.java
@@ -0,0 +1,82 @@
+package exercises.K2;
+
+import java.util.Scanner;
+
+public class ExamCashMachine {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		do {
+			System.out.print("Kontonummer: ");
+			String kontonummer = scanner.next();
+
+			System.out.print("PIN: ");
+			short pin = scanner.nextShort();
+
+			boolean login = checkLoginData(kontonummer, pin);
+
+			if (login) {
+				short auswahl;
+				do {
+					System.out
+							.print("Auswahl treffen: \n1-Kontostand anzeigen,\n2-Geld abheben,\n3-Beenden\nEingabe: ");
+					auswahl = scanner.nextShort();
+					switch (auswahl) {
+						case 1:
+							showBankBalance();
+							break;
+
+						case 2:
+							System.out.print("Betrag: ");
+							short betrag = scanner.nextShort();
+							boolean habensaldo = checkBankBalance(betrag);
+
+							if (habensaldo) {
+								payMoney(betrag);
+								break;
+							} else {
+								System.out.println("Kontostand zu niedrig");
+								break;
+							}
+
+						default:
+							break;
+					}
+				} while (auswahl != 3);
+
+			} else {
+				System.out.println("Logindaten ungültig");
+			}
+		} while (true);
+
+	}
+
+	public static boolean checkLoginData(String kontonummer, short pin) {
+		// Dummy implementation for login check
+		if (kontonummer.equals("12345") && pin == 1234) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void showBankBalance() {
+		// Dummy implementation for showing bank balance
+		System.out.println("Ihr Kontostand beträgt 1000 Euro");
+	}
+
+	public static boolean checkBankBalance(short betrag) {
+		// Dummy implementation for balance check
+		int currentBalance = 1000; // Example balance
+		if (currentBalance >= betrag) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void payMoney(short betrag) {
+		// Dummy implementation for paying money
+		System.out.println(betrag + " Euro wurden abgehoben");
+	}
+}
---
+++ scr/exercises/K2/ExamTask03.java
@@ -0,0 +1,51 @@
+package exercises.K2;
+
+import java.util.Random;
+
+public class ExamTask03 {
+    private static int counter;
+    private static int[] data;
+
+    public static void main(String[] args) {
+
+        Random myRandom = new Random();
+        data = new int[1_000_001];
+
+        for (int i = 0; i < 1_000_000; i++) {
+            data[i] = myRandom.nextInt(100) + 1;
+
+        }
+
+        insertSort();
+
+    }
+
+    private static void insertSort() {
+        int tmp = 0;
+        int i = 1;
+        while (i < data.length) {
+            counter++;
+            int j = i;
+            tmp = data[j];
+
+            while (j > 0 && data[j - 1] > tmp) {
+                data[j] = data[j - 1];
+                j--;
+            }
+
+            data[j] = tmp;
+            print();
+            i++;
+        }
+
+    }
+
+    private static void print() {
+        System.out.println("Duchlauf: " + counter);
+        int i = 0;
+        while (i < data.length) {
+            System.out.println("Ausgabe:" + data[i]);
+            i++;
+        }
+    }
+}
